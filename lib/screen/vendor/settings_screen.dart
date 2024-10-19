bed4f6e1577a7b56c5363090016578a3d2a29763
ivoidable <invoidable@gmail.com>
2024-10-19 22:23:52 +0300

    Created settings_screen.dart

---
+++ lib/screen/vendor/settings_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class VendorSettingsScreen extends StatelessWidget {
+  const VendorSettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/screen/vendor/vendor_tabs.dart
+++ lib/screen/vendor/vendor_tabs.dart
@@ -1,41 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:vendor/helper/helper_widgets.dart';
+import 'package:vendor/model/event.dart';
 
 class VendorEventsTab extends StatelessWidget {
-  final List<Map<String, dynamic>> events = [
-    {
-      'name': 'Food Festival',
-      'image': 'https://via.placeholder.com/150',
-      'vendors': 12,
-      'maxVendors': 20,
-    },
-    {
-      'name': 'Tech Expo',
-      'image': 'https://via.placeholder.com/150',
-      'vendors': 8,
-      'maxVendors': 15,
-    },
-  ];
+  final List<Event> events = [];
   //TODO: Fetch Real Event Data
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(8.0),
-      itemCount: events.length,
-      itemBuilder: (context, index) {
-        final event = events[index];
-        return EventCard(
-          name: event['name'],
-          imageUrl: event['image'],
-          vendors: event['vendors'],
-          maxVendors: event['maxVendors'],
-          onClick: () {
-            // Handle click event
-            print('Clicked on ${event['name']}');
-          },
-        );
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: ListView.builder(
+        padding: const EdgeInsets.all(8.0),
+        itemCount: events.length,
+        itemBuilder: (context, index) {
+          final event = events[index];
+          return EventCard(
+            name: event.name,
+            imageUrl: event.imageUrl,
+            vendors: event.registeredVendors.length,
+            maxVendors: event.maxVendors,
+            onClick: () {
+              // Handle click event
+              print('Clicked on ${event.name}');
+            },
+          );
+        },
+      ),
     );
+  }
+}
+
+class VendorMessagesTabs extends StatelessWidget {
+  const VendorMessagesTabs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Messages Tabs");
+  }
+}
+
+class VendorNotificationsTab extends StatelessWidget {
+  const VendorNotificationsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Notifications Tab");
+  }
+}
+
+class VendorProfileTab extends StatelessWidget {
+  const VendorProfileTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Profile Tab");
   }
 }
